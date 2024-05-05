a499b133f06362ce4a402f07b141f0b54a22e996
malakosamaa <152822296+malakosamaa@users.noreply.github.com>
2024-05-05 23:36:47 +0300

    Update Paste.h

--- Phase2 - Code F22/Actions/Paste.h
+++ Phase2 - Code F22/Actions/Paste.h
@@ -1,1 +1,12 @@
+#pragma once
+#include "Actions/Action.h"
+class Paste: public Action
+{
+private:
+	Point pClicked;
+public:
+	Paste(ApplicationManager* pApp);
+	virtual void ReadActionParameters();
+	virtual void Execute();
+};
 
