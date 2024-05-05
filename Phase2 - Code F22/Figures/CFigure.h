621ce94a236634955c0139e38790dd1edbfaf38e
malakosamaa <152822296+malakosamaa@users.noreply.github.com>
2024-05-06 00:08:28 +0300

    Update CFigure.h

--- Phase2 - Code F22/Figures/CFigure.h
+++ Phase2 - Code F22/Figures/CFigure.h
@@ -19,7 +19,7 @@ public:
 
 	void SetSelected(bool s);	//select/unselect the figure
 	bool IsSelected() const;	//check whether fig is selected
-
+virtual void getCopy();
 	virtual void Draw(Output* pOut) const  = 0 ;		//Draw the figure
 	
 	void ChngDrawClr(color Dclr);	//changes the figure's drawing color
