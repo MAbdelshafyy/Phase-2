8abfdc14e580a3f822361fb6505e509fcb84e9be
malakosamaa <152822296+malakosamaa@users.noreply.github.com>
2024-05-09 13:54:00 +0300

    Update Copy.cpp

--- Phase2 - Code F22/Actions/Copy.cpp
+++ Phase2 - Code F22/Actions/Copy.cpp
@@ -2,27 +2,40 @@
 #include"GUI/Input.h"
 #include"GUI/Output.h"
 #include"ApplicationManager.h"
+#include"SelectAction.h"
+#include"Figures/CFigure.h"
 
-Copy::Copy(ApplicationManager* pApp):Action(pApp) {}
+Copy::Copy(ApplicationManager* pApp) :Action(pApp) {}
 
 void Copy::ReadActionParameters() {}
 
-void Copy::Execute() {
+void Copy::Execute()
+{
 	Output* pOut = pManager->GetOutput();
-	CFigure* selectedFig = pManager->getSelectedFig(selectedFig);
-
-
-	if(selectedFig == NULL) //beyshof hwa fe selected figure wala la
+	for (int i = 0; i < pManager->GetFigCount(); i++)
 	{
+		CFigure* SelectFig = pManager->GetSelectedFigList(i);//bye2ra el ragel medakhal eh
+
+		if (SelectFig == NULL) //beyshof hwa fe selected figure wala la
+
 			pOut->PrintMessage("No figure is selected to copy");
-	}
-	else
-	{
-		pManager->setClipboard(selectedFig);
-		pOut->PrintMessage("Selected figure is copied");
 
-		pManager->setSelectedFig(NULL);
-		selectedFig->SetSelected(false); //bey unselect el fig 	
+		else
+		{
+			for (int i = 0; i < pManager->GetFigCount(); i++)
+			{
+				CFigure* SelectFig = pManager->GetSelectedFigList(i);
+
+				pManager->setClipboard(SelectFig);
+
+				pOut->PrintMessage("SeleActed figure is copied");
+
+				pManager->GetFigList(i)->SetSelected(false);
+
+				SelectAction* SelectedFigCount = 0;
+				pOut->PrintMessage("All figures unselected");
+			}
+		}
+
 	}
-	
 }
