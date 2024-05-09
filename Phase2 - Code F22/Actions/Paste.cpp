287406a38e0ec42fb047593d196337d610ebad67
malakosamaa <152822296+malakosamaa@users.noreply.github.com>
2024-05-09 14:03:19 +0300

    Update Paste.cpp

--- Phase2 - Code F22/Actions/Paste.cpp
+++ Phase2 - Code F22/Actions/Paste.cpp
@@ -2,10 +2,14 @@
 #include"ApplicationManager.h"
 #include"GUI/Input.h"
 #include"GUI/Output.h"
+#include"CTriangle.h"
+#include"CCircle.h"
+#include"CHexagon.h"
+#include"CSquare.h"
 
 Paste::Paste(ApplicationManager* pApp) :Action(pApp) {}
 
-void Paste::ReadActionParameters() {  
+void Paste::ReadActionParameters() {
 	Output* pOut = pManager->GetOutput();
 	Input* pIn = pManager->GetInput();
 
@@ -14,67 +18,67 @@ void Paste::ReadActionParameters() {
 	pIn->GetPointClicked(pClicked.x, pClicked.y);
 }
 
-void Paste::Execute() {
+void Paste::Execute()
+{
 	Output* pOut = pManager->GetOutput();
 	CFigure* clipboard = pManager->getClipboard(clipboard);
 
-	if(clipboard==NULL) {
-		pOut->PrintMessage("Pasting failed, Clipboard is empty");
-	}
-	else
-	{
-		ReadActionParameters();
+	CFigure* movecrdnts(Point pClicked, Point p2, Point p3);
 
-		if (!(pClicked.y >= UI.ToolBarHeight && pClicked.y < UI.height - UI.StatusBarHeight))//beyshof el point fe range el drawing area wala la
+	//hastakhdem hena el dynamic cast 3ashan aghayar el pointers beta3et el CFigures akhaleeha pointer el paste
+
+	if (dynamic_cast<CSquare*>(clipboard))		//bey check anhy figure fel clipboard
+	{ 
+			Point p2;
+			Point p3;
+			
+			clipboard->movecrdnts(pClicked, p2, p3);
+
+			CSquare* S = new CSquare(pClicked, p2, clipboard->getGfxInfo());//bye3mel line gedeed bel crdnts el gedeeda bel dynamic allocation eashan yeb2a "copied"
+
+
+			pManager->AddFigure(S); //bey add el fig el gedeed lel list
+
+			pOut->PrintMessage("Square is pasted");
+
+	}
+		else if (dynamic_cast<CTriangle*>(clipboard))
 		{
-			pOut->PrintMessage("The clicked point is not in the drawing area");
-			return;
+			Point p2;
+			Point p3;
+
+			clipboard->movecrdnts(pClicked, p2, p3);
+
+			CTriangle* T = new CTriangle(pClicked, p2, p3, clipboard->getGfxInfo());//same tri bas crdnts gedeeda
+
+			pManager->AddFigure(T);
+
+			pOut->PrintMessage("Triangle is pasted ");
 		}
-		//hastakhdem hena el dynamic cast 3ashan aghayar el pointers beta3et el CFigures akhaleeha pointer el paste
-			if (dynamic_cast<CSquare*>(clipboard)) { //bey check anhy figure fel clipboard
-		Point p2;
-		Point p3;
-		clipboard->movecrdnts(pClicked, p2, p3);
+		else if (dynamic_cast<CCircle*>(clipboard)) 
+		{
+			Point p2;
+			Point p3;
 
-		CSquare* S = new CSquare(pClicked, p2, clipboard->getGfxInfo()); //bye3mel line gedeed bel crdnts el gedeeda bel dynamic allocation eashan yeb2a "copied"
+			clipboard->movecrdnts(pClicked, p2, p3);
 
+			CCircle* H = new CCircle(p2,p3,clipboard->getGfxInfo());//same circ bas crdnts gedeeda
 
-		pManager->AddFigure(S); //bey add el fig el gedeed lel list
+			pManager->AddFigure(H);
 
-		pOut->PrintMessage("Square is pasted");
+			pOut->PrintMessage("Circle is pasted ");
+		}
+		else 
+		{
 
-	}
-	else if (dynamic_cast<CTriangle*>(clipboard)) {
-		Point p2;
-		Point p3;
+			Point p2;
+			Point p3;
+			clipboard->movecrdnts(pClicked, p2, p3);
 
-		clipboard->movecrdnts(pClicked, p2, p3);
+			CHexagon* H = new CHexagon(pClicked,p2,p3, clipboard->getGfxInfo());//same hexa bas crdnts gedeeda
 
-		CTriangle* T = new CTriangle(pClicked, p2, p3, clipboard->getGfxInfo());//same tri bas crdnts gedeeda
+			pManager->AddFigure(H);
 
-		pManager->AddFigure(T);
-
-		pOut->PrintMessage("Triangle is pasted ");
-	}
-	else if (dynamic_cast<CCircle*>(clipboard)) {
-		Point p2;
-		Point p3;
-
-		clipboard->movecrdnts(pClicked, p2, p3);
-
-		CCircle* H = new CCircle(pClicked, p2, p3, clipboard->getGfxInfo());//same tri bas crdnts gedeeda
-
-		pManager->AddFigure(H);
-
-		pOut->PrintMessage("Circle is pasted ");
-	}
-	else  {
-
-		CHexagon* H = new CHexagon(pClicked, p2, p3, clipboard->getGfxInfo());//same tri bas crdnts gedeeda
-
-		pManager->AddFigure(H);
-
-		pOut->PrintMessage("Hexagon is pasted ");
-	}
-	
+			pOut->PrintMessage("Hexagon is pasted ");
+		}
 }
