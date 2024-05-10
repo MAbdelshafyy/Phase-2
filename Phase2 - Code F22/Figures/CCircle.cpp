e90e8b8d61c2f4468ac033acec6edd850318d55f
malakosamaa <152822296+malakosamaa@users.noreply.github.com>
2024-05-10 18:57:14 +0300

    Update CCircle.cpp

--- Phase2 - Code F22/Figures/CCircle.cpp
+++ Phase2 - Code F22/Figures/CCircle.cpp
@@ -1,13 +1,34 @@
 #include "CCircle.h"
+#include <cmath>
+#include "CCircle.h"
 
-CCircle::CCircle(Point, Point, GfxInfo FigureGfxInfo):CFigure(FigureGfxInfo) ///Initializer for constructor "inherited class"
+
+CCircle::CCircle(Point Point1,Point Point2, GfxInfo FigureGfxInfo):CFigure(FigureGfxInfo) ///Initializer for constructor "inherited class"
 {
 	P1 = Point1;
 	P2 = Point2;
 }
 
+
 void CCircle::Draw(Output* pOut) const
 {
-	//Call Output::DrawCirc to draw a square on the screen
+	//Call Output::DrawCirc to draw a circle on the screen
 	pOut->DrawCirc(P1, P2, FigGfxInfo, Selected);
+}
+
+bool CCircle::PointIn(Point p)
+{
+	Point C;
+	C.x = (P1.x + P2.x) / 2;
+	C.y = (P1.y + P2.y) / 2;
+	double d = sqrt((p.x - C.x) * (p.x - C.x) + (p.y - C.y) * (p.y - C.y));
+	double r =sqrt( ((P2.x - P1.x) * (P2.x - P1.x) + (P2.y - P1.y) * (P2.y - P1.y)))/2;
+	if (d <= r)
+		return true;
+	else
+		return false;
+}
+
+void CCircle::movecrdnts(Point pClicked, Point p2, Point p3)
+{
 }
