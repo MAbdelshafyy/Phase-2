2f7d997fdb61e0607131b91062f8513ba3cf909a
malakosamaa <152822296+malakosamaa@users.noreply.github.com>
2024-05-10 19:03:13 +0300

    Update CSquare.h

--- Phase2 - Code F22/Figures/CSquare.h
+++ Phase2 - Code F22/Figures/CSquare.h
@@ -10,6 +10,8 @@ private:
 public:
 	CSquare(Point, GfxInfo FigureGfxInfo);
 	virtual void Draw(Output* pOut) const;
+	virtual void movecrdnts(Point pClicked, Point p2, Point p3) = 0;
+
 };
 
 #endif
