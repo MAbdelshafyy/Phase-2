768ad09d72f0e98f5d6f247fb5168d4e9182e974
malakosamaa <152822296+malakosamaa@users.noreply.github.com>
2024-05-10 19:00:41 +0300

    Update CHexagon.h

--- Phase2 - Code F22/Figures/CHexagon.h
+++ Phase2 - Code F22/Figures/CHexagon.h
@@ -13,6 +13,8 @@ public:
 
 	CHexagon(Point Center, GfxInfo FigureGfxInfo);
 	virtual void Draw(Output* pOut) const;
+	virtual void movecrdnts(Point pClicked, Point p2, Point p3) = 0;
+
 
 };
 #endif
