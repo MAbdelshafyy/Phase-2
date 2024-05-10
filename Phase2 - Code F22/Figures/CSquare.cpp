1ad63bbe14de7c5b99ee460985978454094c46ba
malakosamaa <152822296+malakosamaa@users.noreply.github.com>
2024-05-10 19:02:40 +0300

    Update CSquare.cpp

--- Phase2 - Code F22/Figures/CSquare.cpp
+++ Phase2 - Code F22/Figures/CSquare.cpp
@@ -1,12 +1,25 @@
 #include "CSquare.h"
+#include "CSquare.h"
 
-CSquare::CSquare(Point, GfxInfo FigureGfxInfo)
+CSquare::CSquare(Point Center ,GfxInfo FigureGfxInfo) :CFigure(FigureGfxInfo)
 {
 	Center1 = Center;
 }
+
+
 
 void CSquare::Draw(Output* pOut) const
 {
 	//Call Output::DrawSquare to draw a square on the screen
 	pOut->DrawSquare(Center1, FigGfxInfo, Selected);
+}
+
+bool CSquare::PointIn(Point p)
+{
+	return ((p.x >= Center1.x - 100) && (p.x <= Center1.x + 100) && (p.y >= Center1.y - 100) && (p.y <= Center1.y + 100));
+}
+
+void CSquare::movecrdnts(Point pClicked, Point p2, Point p3)
+{
+
 }
