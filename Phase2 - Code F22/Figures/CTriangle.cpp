7345e8fd4535d2565658c519632de4af550c13d4
malakosamaa <152822296+malakosamaa@users.noreply.github.com>
2024-05-10 19:03:50 +0300

    Update CTriangle.cpp

--- Phase2 - Code F22/Figures/CTriangle.cpp
+++ Phase2 - Code F22/Figures/CTriangle.cpp
@@ -1,6 +1,7 @@
 #include "CTriangle.h"
+#include "CTriangle.h"
 
-CTriangle::CTriangle(Point, Point, Point, GfxInfo FigGfxInfo):CFigure(FigGfxInfo)
+CTriangle::CTriangle(Point Corner1, Point Corner2, Point Corner3, GfxInfo FigGfxInfo) :CFigure(FigGfxInfo)
 {
 	P1 = Corner1;
 	P2 = Corner2;
@@ -12,3 +13,18 @@ void CTriangle::Draw(Output* pOut) const
 	//Call Output::DrawTri to draw a square on the screen
 	pOut->DrawTri(P1, P2, P3, FigGfxInfo, Selected);
 }
+
+bool CTriangle::PointIn(Point p)
+{
+	double A = getTriangleArea(P1, P2, P3);
+	double A1 = getTriangleArea(p, P2, P3);
+	double A2 = getTriangleArea(P1, p, P3);
+	double A3 = getTriangleArea(P1, P2, p);
+	return (A == A1 + A2 + A3);
+}
+
+void CTriangle::movecrdnts(Point pClicked, Point p2, Point p3)
+{
+}
+
+
