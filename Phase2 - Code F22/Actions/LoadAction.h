6ab9795deb7f482a3c30bb2623a7dff6ffecec68
YoussefSalem1 <167449829+YoussefSalem1@users.noreply.github.com>
2024-05-11 19:28:04 +0300

    Update LoadAction.h

--- Phase2 - Code F22/Actions/LoadAction.h
+++ Phase2 - Code F22/Actions/LoadAction.h
@@ -1,1 +1,24 @@
+#pragma once
+#ifndef LOAD_ACTION_H
+#define LOAD_ACTION_H
+#include "Action.h"
+#include <fstream>
+#include <string>
+#include "colors.h"
 
+class LoadAction :public Action
+{
+private:
+	fstream InFile;
+	string FileName;
+	color GetColorObj(string)const;
+public:
+	LoadAction(ApplicationManager* pApp);
+
+	virtual void ReadActionParameters();
+
+	virtual void Execute();
+
+
+};
+#endif
