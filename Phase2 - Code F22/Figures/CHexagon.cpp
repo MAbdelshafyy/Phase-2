c7f4d1724fb7f6cf53d6d6370b41c33066cc7e42
YoussefSalem1 <167449829+YoussefSalem1@users.noreply.github.com>
2024-05-11 22:41:50 +0300

    Update CHexagon.cpp

--- Phase2 - Code F22/Figures/CHexagon.cpp
+++ Phase2 - Code F22/Figures/CHexagon.cpp
@@ -7,7 +7,7 @@ CHexagon::CHexagon(Point Center, GfxInfo FigureGfxInfo) :CFigure(FigureGfxInfo)
 {
 	P1 = Center;
 }
-
+CHexagon::CHexagon(){}
 void CHexagon::Draw(Output* pOut) const
 {
 	//Call Output::DrawHexagon to draw a square on the screen
